f7551d2b6976974bf0860d78b20030b0547188ef
Ifechukwu Peter <ifecko2dumbi@gmail.com>
2024-07-06 11:27:28 +0100

    updated the dropdown menu with links to respective pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1> Welcome to Jumia</h1>
+      <p>
+        Type your e-mail or phone number to log in or create a Jumia account.
+      </p>
+    </div>
+  );
+}
+
+export default Page;
--- src/components/core/dropdown.tsx
+++ src/components/core/dropdown.tsx
@@ -1,4 +1,12 @@
-import { LuLogOut, LuHeart, LuSettings, LuUser, LuBook } from "react-icons/lu";
+"use client";
+import {
+  LuLogOut,
+  LuHeart,
+  LuSettings,
+  LuUser,
+  LuBook,
+  LuLogIn,
+} from "react-icons/lu";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,8 +19,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { FaAngleDown, FaUser } from "react-icons/fa6";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export function Dropdown() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -28,32 +41,55 @@ export function Dropdown() {
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem className="cursor-pointer">
+          <Link
+            className=" hover:bg-slate-300 cursor-pointer flex flex-row h-10 w-full items-center gap-x-0 px-3 rounded-md"
+            href="/profile"
+          >
             <LuUser className="mr-2 h-4 w-4" />
             <span>Profile</span>
-          </DropdownMenuItem>
+          </Link>
 
           <DropdownMenuItem className="cursor-pointer">
             <LuSettings className="mr-2 h-4 w-4" />
             <span>Settings</span>
           </DropdownMenuItem>
 
-          <DropdownMenuItem className="cursor-pointer">
+          <Link
+            className=" hover:bg-slate-300 cursor-pointer flex flex-row h-10 w-full items-center gap-x-0 px-3 rounded-md"
+            href="/orders"
+          >
             <LuBook className="mr-2 h-4 w-4" />
             <span>My orders</span>
-          </DropdownMenuItem>
+          </Link>
 
-          <DropdownMenuItem className="cursor-pointer">
+          <Link
+            className=" hover:bg-slate-300 cursor-pointer flex flex-row h-10 w-full items-center gap-x-0 px-3 rounded-md"
+            href="/favourites"
+          >
             <LuHeart className="mr-2 h-4 w-4" />
             <span>Favorites</span>
-          </DropdownMenuItem>
+          </Link>
         </DropdownMenuGroup>
 
         <DropdownMenuSeparator />
-        <DropdownMenuItem className=" bg-purple-600 text-white hover:bg-purple-800 cursor-pointer">
-          <LuLogOut className="mr-2 h-5 w-4 hover:bg-purple-800" />
-          <span>Log out</span>
-        </DropdownMenuItem>
+
+        {isLoggedIn ? (
+          <Link
+            className="bg-red-500 text-white hover:bg-red-700 cursor-pointer flex flex-row h-10 w-full items-center gap-x-3 px-3 rounded-md"
+            href="/login"
+          >
+            <LuLogOut />
+            <span>Log out</span>
+          </Link>
+        ) : (
+          <Link
+            className="bg-purple-600 text-white hover:bg-purple-800 cursor-pointer flex flex-row h-10 w-full items-center gap-x-3 px-3 rounded-md"
+            href="/login"
+          >
+            <LuLogIn />
+            <span>Log in</span>
+          </Link>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
